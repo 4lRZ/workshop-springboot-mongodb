6128c3b242613d6ddc34f173003abbe71cdf4d46
Alvaro Dormundo <adormundo@gmail.com>
2022-03-21 11:44:13 -0300

    Database instantiation

---
+++ src/main/java/com/alrz/workshop/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.alrz.workshop.config;
+
+import com.alrz.workshop.domain.User;
+import com.alrz.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
