4ee8a094517bea1a69f8184fd8e943bfbe892a2c
Alvaro Dormundo <adormundo@gmail.com>
2022-03-22 01:30:11 -0300

    Create entity Post with User

--- src/main/java/com/alrz/workshop/config/Instantiation.java
+++ src/main/java/com/alrz/workshop/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.alrz.workshop.config;
 
+import com.alrz.workshop.domain.Post;
 import com.alrz.workshop.domain.User;
+import com.alrz.workshop.repository.PostRepository;
 import com.alrz.workshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +18,23 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository PostRepository;
+
     @Override
     public void run(String... args) throws Exception {
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         userRepository.deleteAll();
+        PostRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        PostRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/alrz/workshop/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.alrz.workshop.repository;
+
+import com.alrz.workshop.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
