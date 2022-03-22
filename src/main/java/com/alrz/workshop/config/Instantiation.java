48876232322675476abfae5026966e5fd756147c
Alvaro Dormundo <adormundo@gmail.com>
2022-03-22 02:25:14 -0300

    Adding comments to posts

--- src/main/java/com/alrz/workshop/config/Instantiation.java
+++ src/main/java/com/alrz/workshop/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.alrz.workshop.config;
 import com.alrz.workshop.domain.Post;
 import com.alrz.workshop.domain.User;
 import com.alrz.workshop.dto.AuthorDTO;
+import com.alrz.workshop.dto.CommentDTO;
 import com.alrz.workshop.repository.PostRepository;
 import com.alrz.workshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +35,14 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
         PostRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPost().addAll(Arrays.asList(post1,post2));
